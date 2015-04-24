e75eecff17922cd8c57f01f0647b1c621cd9b6ee
Daniel Hiller <dhiller@wolterskluwer.de>
2015-04-24 13:22:33 +0200

    New scripts added

---
+++ updateIndex.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -e
+
+CWD=$(pwd)
+
+cd /var/opengrok/src/
+
+curl "http://stash/rest/api/1.0/projects" > temp.txt
+for PROJECT_URL in $(grep -e "\"href\":\"[^\"]\+\"" -o temp.txt | sed s/^\"href\":\"// | sed s/\"$//); do
+    PROJECT_KEY=${PROJECT_URL##*/}
+    curl "http://stash/rest/api/1.0/projects/$PROJECT_KEY/repos" > "$PROJECT_KEY.txt"
+    for REPO_URL in $(grep -e "http:[^\"]\+\.git" -o "$PROJECT_KEY.txt"); do
+        PROJECT_DIR=${REPO_URL##*/}
+        PROJECT_DIR=${PROJECT_DIR%.git}
+        if [ ! -d "$PROJECT_DIR" ]; then
+            git clone "$REPO_URL"
+        else
+            cd "$PROJECT_DIR"
+            git pull
+            cd ..
+        fi
+    done
+    rm -f "$PROJECT_KEY.txt"
+done
+
+rm -f temp.txt
+
+/usr/share/opengrok-0.12.1.1/bin/OpenGrok index
+
+cd "$CWD"
