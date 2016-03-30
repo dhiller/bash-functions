551b356761831b40ce4a3b1465045415acef2a4e
Daniel Hiller <dhiller@wolterskluwer.de>
2016-03-30 09:57:38 +0200

    Fix: use mktemp

--- opengrok/updateIndex.sh
+++ opengrok/updateIndex.sh
@@ -4,35 +4,39 @@ set -e
 
 CWD=$(pwd)
 
-# Fetch and/or update all public stash repositories
+# Fetch and/or update all public bitbucket repositories
 
 cd /opt/opengrok/src/
 
-curl "http://stash.wkd.wolterskluwer.de/rest/api/1.0/projects" > temp.txt
-for PROJECT_URL in $(grep -e "\"href\":\"[^\"]\+\"" -o temp.txt | sed s/^\"href\":\"// | sed s/\"$//); do
+TMP_PROJECTS=$(mktemp)
+curl "http://bitbucket.wkd.wolterskluwer.de/rest/api/1.0/projects" > $TMP_PROJECTS
+for PROJECT_URL in $(grep -e "\"href\":\"[^\"]\+\"" -o $TMP_PROJECTS | sed s/^\"href\":\"// | sed s/\"$//); do
     PROJECT_KEY=${PROJECT_URL##*/}
-    curl "http://stash/rest/api/1.0/projects/$PROJECT_KEY/repos" > "$PROJECT_KEY.txt"
-    for REPO_URL in $(grep -e "http:[^\"]\+\.git" -o "$PROJECT_KEY.txt"); do
+    TMP_REPOSITORIES=$(mktemp)
+    curl "http://bitbucket.wkd.wolterskluwer.de/rest/api/1.0/projects/$PROJECT_KEY/repos" > "$TMP_REPOSITORIES"
+    for REPO_URL in $(grep -e "http:[^\"]\+\.git" -o "$TMP_REPOSITORIES"); do
         PROJECT_DIR=${REPO_URL##*/}
         PROJECT_DIR=${PROJECT_DIR%.git}
         if [ ! -d "$PROJECT_DIR" ]; then
             git clone "$REPO_URL"
         else
             cd "$PROJECT_DIR"
+            set +e
             git fetch --all
             git pull
+            set -e
             cd ..
         fi
     done
-    rm -f "$PROJECT_KEY.txt"
+    rm -f "$TMP_REPOSITORIES"
 done
 
-rm -f temp.txt
+rm -f $TMP_PROJECTS
 
 #./updateSVNRepos.sh
 
 # rebuild indices
-/opt/opengrok/bin/OpenGrok index
+JAVA_OPTS="-Xmx4G" /opt/opengrok/bin/OpenGrok index
 
 # Reindexing only didn't update list of projects
 #service tomcat restart
